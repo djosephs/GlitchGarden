ec246d4f56fdbe64478ad5382875e28f682e66bd
djosephs <djosephs12@gmail.com>
2017-02-06 23:31:53 -0500

    1

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+	private AudioSource audioSource;
+	public AudioClip[] levelMusicChangeArray;
+	// Use this for initialization
+	void Awake ()
+	{
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destroy on load: " + name);
+	}
+	
+	
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		
+	}
+	// Update is called once per frame
+	void OnLevelWasLoaded (int level)
+	{
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		Debug.Log ("Playing clip: " + thisLevelMusic);
+		if (thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
