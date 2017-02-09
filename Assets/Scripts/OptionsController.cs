e53e4c36c196219bd176d975084f544b8020dd0c
djosephs <djosephs12@gmail.com>
2017-02-09 10:18:10 -0500

    7/139

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour
+{
+
+	public Slider volumeSlider;
+	public LevelManager levelManager;
+	private MusicManager musicManager;
+	// Use this for initialization
+	void Start ()
+	{
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+		Debug.Log (musicManager);
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+
+	public void SaveandExit ()
+	{
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+		levelManager.LoadLevel ("01a Start");
+	}
+}
